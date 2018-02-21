137d74b6b58f2feda434c0c971e19ec604876ceb
Ghost <daniel.sendmemail@gmail.com>
2018-02-20 22:23:21 -0500

    Added new elemnts

--- js/seebaly.js
+++ js/seebaly.js
@@ -1,50 +1,16 @@
-var stripe = Stripe('pk_test_0KKZ0oM3k2R3x5bn6TDhWJwd');
+var stripe = Stripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
 var elements = stripe.elements();
 
-var card = elements.create('card', {
-  style: {
+var style = {
     base: {
-      iconColor: '#BCBCBC',
-      color: '#FFFFFF',
-      lineHeight: '40px',
-      fontWeight: 300,
-      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-      fontSize: '15px',
-
-      '::placeholder': {
-        color: '#656565',
-      },
-    },
-  }
-});
-card.mount('#card-element');
-
-function setOutcome(result) {
-  var successElement = document.querySelector('.success');
-  var errorElement = document.querySelector('.error');
-  successElement.classList.remove('visible');
-  errorElement.classList.remove('visible');
-
-  if (result.token) {
-    // Use the token to create a charge or a customer
-    // https://stripe.com/docs/charges
-    successElement.querySelector('.token').textContent = result.token.id;
-    successElement.classList.add('visible');
-  } else if (result.error) {
-    errorElement.textContent = result.error.message;
-    errorElement.classList.add('visible');
-  }
-}
-
-card.on('change', function(event) {
-  setOutcome(event);
-});
-
-document.querySelector('form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  var form = document.querySelector('form');
-  var extraDetails = {
-    name: form.querySelector('input[name=cardholder-name]').value,
+      // Add your base input styles here. For example:
+      fontSize: '16px',
+      color: "#32325d",
+    }
   };
-  stripe.createToken(card, extraDetails).then(setOutcome);
-});
+  
+  // Create an instance of the card Element
+  var card = elements.create('card', {style: style});
+  
+  // Add an instance of the card Element into the `card-element` <div>
+  card.mount('#card-element');
