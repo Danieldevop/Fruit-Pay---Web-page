99a5483a1e811b777109a84bb1c279d7e653cafd
Ghost <daniel.sendmemail@gmail.com>
2018-04-27 17:28:34 -0500

    new design

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer(props) {
   return (
     <footer className="foo text-center">
-      <p>© 2018 Seebaly go international</p>
+      <p>© 2018 moneyX | go international</p>
     </footer>
   )
 }
