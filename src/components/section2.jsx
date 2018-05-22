adbea08c8da81d41e37cbdd7a33e527bdfe52cf9
Ghost <daniel.sendmemail@gmail.com>
2018-05-21 22:55:32 -0500

    images changed

--- src/components/section2.jsx
+++ src/components/section2.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './section2.css'
-import globe from '../img/globe.png'
-import fyf from '../img/fyf.png'
-import merchant from '../img/merchant.png'
+import globe from '../img/globe.svg'
+import fyf from '../img/fyf.svg'
+import merchant from '../img/share.svg'
 import person from '../img/person.png'
 import corporate from '../img/corporate.png'
 
@@ -14,15 +14,15 @@ function Section2(props) {
           <h2 className="text-center p1" >This is MoneyX</h2>
           <div className="row pretext">
             <div className="col-md b1">
-              <img className="img-fluid mx-auto d-block" src={globe} alt="" width="" />
-              <p className="text-center mt-3">Real Time* payments card to card</p>
+              <img className="img-fluid mx-auto d-block" src={globe} alt="" width="90" />
+              <p className="text-center mt-3">Real Time* payments</p>
             </div>
             <div className="col-md b2">
-              <img className="img-fluid mx-auto d-block" src={fyf} alt="" width="100" />
-              <p className="text-left mt-3">Friends and family money transfers in your country and real time remittances.</p>
+              <img className="img-fluid mx-auto d-block" src={fyf} alt="" width="90" />
+              <p className="text-center mt-3">Transfer money to family and friends using your mobile phone.</p>
             </div>
             <div className="col-md b3">
-              <img className="img-fluid mx-auto d-block" src={merchant} alt="" width="" />
+              <img className="img-fluid mx-auto d-block" src={merchant} alt="" width="90" />
               <p className=" mt-3 text-left">Businesses persons and institutions can receive payments avoiding the ACH IBAN and other slow systems.</p>
             </div>
           </div>
@@ -41,7 +41,7 @@ function Section2(props) {
               <p>Pay on websites</p>
               <p>Pay with Splitting bills</p>
               <p>Send remittances</p>
-              <p>Send and receive money from clients, friends and familiars</p>
+              <p>Send and receive money from clients<br/> friends and familiars</p>
             </div>
           </div>
           <h1 className="text-center mb-5 mt-5 tip">Superpowers For Businesses and Goverments</h1>
@@ -50,21 +50,17 @@ function Section2(props) {
               <img className="img-fluid mx-auto d-block " src={corporate} alt="person" width="600" />
             </div>
             <div className="col-md">
-              <p>With our simply checkout you can receive payments worldwide directly to your account </p>
+              <p>Receive payments worldwide directly to your account </p>
               <p>Funds disbursement options for a variety of different situations </p>
-              <p>
-                Reimbursements
-                Refunds
-                Rebates
-                Payouts
-                Loan distributions
-                Government disbursements
-              </p>
+              <p>Manage Refunds</p>
+              <p>Manage Rebates</p>
+              <p>Manage Loan distributions</p>
+              <p>Government disbursements</p>
             </div>
           </div>
         </div>
         <div className="mates"></div>
-        <div className="mates mt-5"></div>
+        <div className="mates mt-5"></div>  
       </section>
     </div>
   )
